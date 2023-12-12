0d57eb191c850aff059fbe5ff7d7b2ad1c1c458b
Ryomen-Sukuna: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@github.com <ryomensukuna83@gmail.com>
2023-12-12 14:06:17 +0700

    add more features (WIP)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//Toggle class active
+// Toggle class active for hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
 
 // Hamburger menu clicked
@@ -6,11 +6,65 @@ document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
-//Click outside the sidebar to remove the navigation bar
-const hamburger = document.querySelector('#hamburger-menu');
+// Toggle class active for search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+// Search form clicked
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+}
+
+// Toggle class active for shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+
+// Shopping cart clicked
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
+}
+
+// Click outside element
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
 
 document.addEventListener('click', function (e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
     }
+
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
+
+    if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active')
+    }
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    }
+})
+
+// Closed Modal Box
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+}
+
+// Click outside modal box
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = 'none';
+    }
+};
